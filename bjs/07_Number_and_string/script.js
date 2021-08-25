0e5e71e706d8a309cc79ea25524be63aba168b32
TixomirSirts <tihomir.sirts@yandex.ru>
2021-08-25 21:33:48 +0300

    Added first ctructure

--- bjs/07_Number_and_string/script.js
+++ bjs/07_Number_and_string/script.js
@@ -4,6 +4,37 @@ let operation = null;
 const inputWindow = document.getElementById('inputWindow');
 
 
+document.getElementById('btn_1').addEventListener('click', function () {
+    inputWindow.value += 1;
+})
+
+document.getElementById('btn_sum').addEventListener('click', function () {
+    lastOperand = parseInt(inputWindow.value);
+    operation = 'sum';
+    inputWindow.value = '';
+})
+
+document.getElementById('btn_dif').addEventListener('click', function () {
+    lastOperand = parseInt(inputWindow.value);
+    operation = 'dif';
+    inputWindow.value = '';    
+})
+
+document.getElementById('btn_calc').addEventListener('click', function () {
+    if (operation === 'sum') {
+        const result = lastOperand + parseInt(inputWindow.value);
+        operation = null;
+        lastOperand = 0;
+        inputWindow.value = result;
+    }
+    if (operation === 'dif') {
+        const result = lastOperand - parseInt(inputWindow.value);
+        operation = null;
+        lastOperand = 0;
+        inputWindow.value = result;
+    }
+})
+
 document.getElementById('btn_clr').addEventListener('click', function () {
     lastOperand = 0;
     operation = null;
