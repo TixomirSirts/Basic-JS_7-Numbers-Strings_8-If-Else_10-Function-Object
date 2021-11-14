b6f951da0af9380c39130c33e9d733abeef3c48c
TixomirSirts <tihomir.sirts@yandex.ru>
2021-11-14 17:45:34 +0300

    Refactoring promt & alert to display-none

--- bjs/08_if_else/script.js
+++ bjs/08_if_else/script.js
@@ -1,35 +1,165 @@
-let minValue = parseInt(prompt('Введите минимальное значение числа для игры', '-999'));
-let maxValue = parseInt(prompt('Введите максимальное значение числа для игры', '999'));
+const newMaxInput = document.getElementById('newMaxInput');
+const newMinInput = document.getElementById('newMinInput');
+const btnLaunch = document.getElementById('btnLaunch');
+const btnLaunchText = document.getElementById('btnLaunchText');
+const gameSettingsText = document.getElementById('gameSettingsText');
+const newLimits = document.getElementById('newLimits');
+const gameInfo = document.getElementsByClassName('gameInfo');
+
+let maxValue = parseInt(newMaxInput.value);
+let minValue = parseInt(newMinInput.value);
+let noError = true;
+
+function preLaunch(func) {
+    btnLaunch.addEventListener('click', func);
+}
+
+function disLaunch(func) {
+    btnLaunch.removeEventListener('click', func);
+}
+
+function gameTurn(turn) {
+    gameSettingsText.innerHTML += `${turn} <br>`;
+    // console.log(turn);
+}
+
+function clearText() {
+    gameSettingsText.innerHTML = '';
+}
+
+function setValue() {
+    maxValue = parseInt(newMaxInput.value);
+    minValue = parseInt(newMinInput.value);
+    // console.log('Set min value to', minValue, '/ Set max value to', maxValue);
+}
+
+function setLimits() {
+    gameMaxLimit = maxValue;
+    gameMinLimit = minValue;
+    gameMinLimitField.innerText = gameMinLimit;
+    gameMaxLimitField.innerText = gameMaxLimit;
+    // console.log('Set min limite on', gameMinLimit, '/ Set max limite on', gameMaxLimit);
+}
 
 function reEnteryLimits() {
-    minValue = parseInt(prompt('Повторно введите минимальное значение числа для игры', '-999'));
-    maxValue = parseInt(prompt('Повтороно введите максимальное значение числа для игры', '999'));
+    const reEnteryText = `<br>Повторно введите минимальное и максимальное значения от -999 до 999.`;
+    gameTurn(reEnteryText);
 }
 
 function mistakeLimits() {
+    // Для проверки работы функции fixLimits необходимо отключить код, проверяющий ошибку ввода.
+    // Начало кода, проверяющего ошибку ввода:
+    if (minValue >= 999 || maxValue > 999 || maxValue < -998) {
+        clearText();
+        if (minValue >= 999) {
+            const inputMinErorr = `Вы ошиблись (min = ${minValue}): <br> минимальное значение можно указать <br> в промежутке от -999 до 998 <br>`;
+            gameTurn(inputMinErorr);
+        }
+        if (maxValue > 999 || maxValue < -998) {
+            const inputMaxErorr = `Вы ошиблись (max = ${maxValue}): <br> максимальное значение можно указать <br> в промежутке от -998 до 999`;
+            gameTurn(inputMaxErorr);
+        }
+        reEnteryLimits();
+        noError = false;
+    } else 
+    // Конец кода, проверяющего ошибку ввода.
     if (minValue > maxValue) {
-        alert(`Вы ошиблись: ( ${minValue} > ${maxValue} ) - минимальное значение должно быть меньше максимального`);
+        const mismatchErorr = `Вы ошиблись (min: ${minValue} > max: ${maxValue}): <br> минимальное значение необходимо указать меньше максимального.`;
+        clearText();
+        gameTurn(mismatchErorr);
         reEnteryLimits();
-    }
-    if (minValue == maxValue) {
-        alert(`Вы ошиблись: ( ${minValue} = ${maxValue} ) - минимальное значение не должно быть равно максимальному`);
+        noError = false;
+    } else if (minValue == maxValue) {
+        const matchErorr = `Вы ошиблись (min: ${minValue} = max: ${maxValue}): <br> минимальное значение необходимо указать неравное максимальному.`;
+        clearText();
+        gameTurn(matchErorr);
         reEnteryLimits();
+        noError = false;
+    } else {
+        noError = true
     }
 }
 
 function fixLimits() {
-    (minValue < -999 || minValue > 998) ? minValue = -999:
-        (minValue == null || '' || undefined) ? minValue = -999 :
-        (isNaN(minValue)) ? minValue = -999 : minValue = minValue;
+    function minFix() {
+        minValue = -999;
+        // console.log('Fixed min to -999');
+    }
+    function maxFix() {
+        maxValue = 999;
+        // console.log('Fixed max to 999');
+    }
+    (minValue < -999 || minValue > 998) ? minFix():
+        (minValue == null || '' || undefined) ? minFix() :
+        (isNaN(minValue)) ? minFix() : minValue = minValue;
 
-    (maxValue > 999 || maxValue < -998) ? maxValue = 999:
-        (maxValue == null || '' || undefined) ? maxValue = 999 :
-        (isNaN(maxValue)) ? maxValue = 999 : maxValue = maxValue;
+    (maxValue > 999 || maxValue < -998) ?  maxFix():
+        (maxValue == null || '' || undefined) ? maxFix() :
+        (isNaN(maxValue)) ? maxFix() : maxValue = maxValue;
 }
 
-mistakeLimits();
-fixLimits();
-alert(`Загадайте любое целое число от ${minValue} до ${maxValue}, а я его угадаю`);
+function discover(id) {
+    let covertItem = document.getElementById(`${id}`);
+    covertItem.classList.add('discovert');
+    covertItem.classList.remove('covert');
+}
+
+function cover(id) {
+    let discovertItem = document.getElementById(`${id}`);
+    discovertItem.classList.add('covert');
+    discovertItem.classList.remove('discovert');
+}
+
+function discoverAll() {
+    let covertNum = document.getElementsByClassName('covert').length;
+    for (let i = (covertNum - 1); i >= 0; i--) {
+        let covertItem = document.getElementsByClassName('covert')[i];
+        covertItem.classList.add('discovert');
+        covertItem.classList.remove('covert');
+        if (i > 10 || i < -10) break;
+    }
+}
+
+function coverAll() {
+    let discovertNum = document.getElementsByClassName('discovert').length;
+    for (let i = (discovertNum - 1); i >= 0; i--) {
+        let discovertItem = document.getElementsByClassName('discovert')[i];
+        discovertItem.classList.add('covert');
+        discovertItem.classList.remove('discovert');
+        if (i > 10 || i < -10) break;
+    }
+}
+
+function coverGS() {
+    cover('gameSettings')
+}
+
+function startGameText() {
+    const startGameText = `Загадайте любое целое число <br> от ${minValue} до ${maxValue}, <br> а я его угадаю.`;
+    clearText();
+    gameTurn(startGameText);
+}
+
+function checkError() {
+    if (noError == true) {
+        disLaunch(setValue);
+        disLaunch(mistakeLimits);
+        disLaunch(checkError);
+        fixLimits();
+        setLimits();
+        newMaxInput.value = '999';
+        newMinInput.value = '-999';
+        newLimits.classList.add('covert');
+        btnLaunch.innerText = `let\'s go!`;
+        startGameText();
+        preLaunch(discoverAll);
+        preLaunch(coverGS);
+    }
+}
+
+preLaunch(setValue);
+preLaunch(mistakeLimits);
+preLaunch(checkError);
 
 let gameMinLimit = minValue;
 let gameMaxLimit = maxValue;
@@ -48,7 +178,9 @@ orderNumberField.innerText = orderNumber;
 answerField.innerText = `Вы загадали число ${answerNumber }?`;
 numToText(answerNumber);
 
-document.getElementById('btnRetry').addEventListener('click', function () {
+document.getElementById('btnRetry').addEventListener('click', retry);
+
+function retry() {
     minValue = gameMinLimit;
     maxValue = gameMaxLimit;
     orderNumber = 0;
@@ -57,7 +189,29 @@ document.getElementById('btnRetry').addEventListener('click', function () {
     gameRun = true;
     orderNumberField.innerText = orderNumber;
     gameMainQuestion();
-})
+}
+
+document.getElementById('btnChangeLimits').addEventListener('click', changeLimits);
+
+function changeLimits() {
+    coverAll();
+    discover('gameSettings');
+    discover('newLimits');
+    newLimitsText();
+    btnLaunch.innerText = `are you ready?`
+    disLaunch(discoverAll);
+    disLaunch(coverGS);
+    preLaunch(setValue);
+    preLaunch(mistakeLimits);
+    preLaunch(checkError);
+    preLaunch(retry);
+}
+
+function newLimitsText() {
+    const newLimitsText =`Введите новые значения для игры<br>min и max от -999 до 999.`;
+    clearText();
+    gameTurn(newLimitsText);
+}
 
 function gameMainQuestion() {
     const phraseRandom = Math.round(Math.random() * 3);
@@ -78,25 +232,6 @@ function gameMainQuestion() {
     answerField.innerText = questionPhrase;
     numToText(answerNumber);
 }
-
-
-document.getElementById('btnChangeLimits').addEventListener('click', function () {
-    orderNumber = 0;
-    minValue = parseInt(prompt(`Текущее минимальное значение числа для игры: ${gameMinLimit}`, `${gameMinLimit}`));
-    maxValue = parseInt(prompt(`Текущее максимальное значение числа для игры: ${gameMaxLimit}`, `${gameMaxLimit}`));
-    mistakeLimits();
-    fixLimits();
-    gameMinLimit = minValue;
-    gameMaxLimit = maxValue;
-    alert(`Загадайте любое целое число от ${minValue} до ${maxValue}, а я его угадаю`);
-    gameMinLimitField.innerText = gameMinLimit;
-    gameMaxLimitField.innerText = gameMaxLimit;
-    answerNumber = Math.floor((minValue + maxValue) / 2);
-    orderNumber = 1;
-    gameRun = true;
-    orderNumberField.innerText = orderNumber;
-    gameMainQuestion();
-})
 
 function gameRightAnswers() {
     const phraseRandom = Math.round(Math.random() * 3);
@@ -146,7 +281,6 @@ function findUpNum() {
     orderNumberField.innerText = orderNumber;
     gameMainQuestion();
     numToText(answerNumber);
-    // console.log(`${minValue} < ${answerNumber} < ${maxValue}`);
 }
 
 function findLowNum() {
@@ -156,7 +290,6 @@ function findLowNum() {
     orderNumberField.innerText = orderNumber;
     gameMainQuestion();
     numToText(answerNumber);
-    // console.log(`${minValue} < ${answerNumber} < ${maxValue}`);
 }
 
 document.getElementById('btnOver').addEventListener('click', function () {
@@ -174,20 +307,20 @@ document.getElementById('btnLess').addEventListener('click', function () {
 })
 
 document.getElementById('btnEqual').addEventListener('click', function () {
-    numToText(answerNumber);
     if (gameRun) {
         gameRightAnswers();
+        numToText(answerNumber);
     }
 })
 
-let onesArr = ["", " один", " два", " три", " четыре", " пять", " шесть", " семь", " восемь", " девять"];
-let tenTwoTenArr = [" десять", " одиннадцать", " двенадцать", " тринадцать", " четырнадцать", " пятнадцать", " шестнадцать", " семнадцать", " восемнадцать", " девятнадцать"];
-let tensArr = [" двадцать", " тридцать", " сорок", " пятьдесят", " шестьдесят", " семьдесят", " восемьдесят", " девяносто"];
-let hundredsArr = [" сто", " двести", " триста", " четыреста", " пятьсот", " шестьсот", " семьсот", " восемьсот", " девятьсот"];
-
 function numToText(answerNumber) {
+    let onesArr = ["", " один", " два", " три", " четыре", " пять", " шесть", " семь", " восемь", " девять"];
+    let tenTwoTenArr = [" десять", " одиннадцать", " двенадцать", " тринадцать", " четырнадцать", " пятнадцать", " шестнадцать", " семнадцать", " восемнадцать", " девятнадцать"];
+    let tensArr = [" двадцать", " тридцать", " сорок", " пятьдесят", " шестьдесят", " семьдесят", " восемьдесят", " девяносто"];
+    let hundredsArr = [" сто", " двести", " триста", " четыреста", " пятьсот", " шестьсот", " семьсот", " восемьсот", " девятьсот"];
     let keyNum = answerNumber;
     let numText = "";
+
     function swithToText(numText) {
         if (numText.length <= 20) {
             let oldText = answerField.innerText;
@@ -224,7 +357,7 @@ function numToText(answerNumber) {
                 swithToText(numText);
             } else if ((keyNum % 10) === 0) {
                 numText += hundredsArr[`${Math.floor(keyNum / 100) - 1}`];
-                ((keyNum % 100) === 10) ?  numText += tenTwoTenArr[0] : numText += tensArr[`${(keyNum % 100) / 10 - 2}`];
+                ((keyNum % 100) === 10) ? numText += tenTwoTenArr[0]: numText += tensArr[`${(keyNum % 100) / 10 - 2}`];
                 swithToText(numText);
             } else {
                 numText += hundredsArr[`${Math.floor(keyNum / 100) - 1}`];
